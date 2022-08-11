8c29b651bd729a7a27452946bdb7f87e1d5ef1ad
Langbein, Michael <michael.langbein@dlr.de>
2022-08-11 20:50:04 +0200

    started work on database

---
+++ src/tdl/utils/database/TaskTreeDB.java
@@ -0,0 +1,19 @@
+package tdl.utils.database;
+
+
+
+public class TaskTreeDB {
+
+    private Database db;
+
+    TaskTreeDB() {
+        this.db = new Database("");
+    }
+
+    public Task getTask(int id) {}
+
+    public Task getSubtree(int id) {}
+
+    public void updateTask(Task task) {}
+
+}
