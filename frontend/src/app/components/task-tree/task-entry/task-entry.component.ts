34cf4a1318c67640996f3f21e2d0170540626844
Langbein, Michael <michael.langbein@dlr.de>
2022-08-17 13:23:27 +0200

    added context menu

--- frontend/src/app/components/task-tree/task-entry/task-entry.component.ts
+++ frontend/src/app/components/task-tree/task-entry/task-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 import { Task } from 'src/app/services/task.service';
 
 @Component({
@@ -10,10 +10,25 @@ export class TaskEntryComponent implements OnInit {
 
   @Input() tree: Task | null = null;
   @Input() active: Task | null = null;
+  showContextMenu = false;
 
-  constructor() { }
+  constructor(private eRef: ElementRef) { }
 
   ngOnInit(): void {
+    // preventing browser's default context menu from popping up on right-click
+    this.eRef.nativeElement.addEventListener('contextmenu', (event: any) => event.preventDefault());
   }
 
+  // checking for clicks outside ot the element
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: any) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.showContextMenu = false;
+    }
+  }
+
+  createChildTask()  {
+    this.showContextMenu = false;
+    console.log("creating child task")
+  }
 }
