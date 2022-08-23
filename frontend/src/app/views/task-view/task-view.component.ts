c008e1a7fcaf42a3efabc4f780116f06a92558dd
Langbein, Michael <michael.langbein@dlr.de>
2022-08-23 10:54:37 +0200

    now showing summary upon completion of a task

--- frontend/src/app/views/task-view/task-view.component.ts
+++ frontend/src/app/views/task-view/task-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService, TaskTree } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-task-view',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskViewComponent implements OnInit {
 
-  constructor() { }
+  public showCompletedModal = false;
+  public lastCompletedTask: TaskTree | null = null;
+
+  constructor(private taskSvc: TaskService) { }
 
   ngOnInit(): void {
+    let lastTask: TaskTree | null = null;
+    this.taskSvc.watchCurrentTask().subscribe(task => {
+      if (task) {
+        if (lastTask) {
+          if (task.completed && task.id === lastTask.id && lastTask.completed !== task.completed) {
+            this.showCompletedModal = true;
+            this.lastCompletedTask = task;
+          }
+        }
+
+        lastTask = { ... task };
+      }
+    })
   }
 
 }
