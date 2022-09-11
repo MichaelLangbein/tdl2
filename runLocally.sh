9b2e0e88d3aa946db7817ae3ee56e95094d584f3
michaellangbein <michael_langbein@hotmail.de>
2022-09-11 12:00:40 +0200

    linux start script

---
+++ runLocally.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+thisDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
+read -n 1 -p "Compile first? [y/N] " compile
+if [[ $compile == "y" || $compile == "Y" ]]; then
+    cd $thisDir/backend
+    npm ci
+    npm run build
+    cd $thisDir/frontend
+    npm ci
+    npm run build
+fi
+
+cd $thisDir/frontend
+gnome-terminal -- npm run host:locally
+cd $thisDir/backend
+gnome-terminal -- npm run run:prod
+
+
